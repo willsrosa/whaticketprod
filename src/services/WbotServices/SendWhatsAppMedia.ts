2a064634a43ab32de9e00e048406cb4a69d68079
willsrosa <33320360+willsrosa@users.noreply.github.com>
2022-01-21 14:14:57 -0300

    primeiro commit
    primeiro commit

---
+++ src/services/WbotServices/SendWhatsAppMedia.ts
@@ -0,0 +1,43 @@
+import fs from "fs";
+import { MessageMedia, Message as WbotMessage } from "whatsapp-web.js";
+import AppError from "../../errors/AppError";
+import GetTicketWbot from "../../helpers/GetTicketWbot";
+import Ticket from "../../models/Ticket";
+
+import formatBody from "../../helpers/Mustache";
+
+interface Request {
+  media: Express.Multer.File;
+  ticket: Ticket;
+  body?: string;
+}
+
+const SendWhatsAppMedia = async ({
+  media,
+  ticket,
+  body
+}: Request): Promise<WbotMessage> => {
+  try {
+    const wbot = await GetTicketWbot(ticket);
+
+    const newMedia = MessageMedia.fromFilePath(media.path);
+    const sentMessage = await wbot.sendMessage(
+      `${ticket.contact.number}@${ticket.isGroup ? "g" : "c"}.us`,
+      newMedia,
+      {
+        caption: formatBody(body as string, ticket.contact),
+        sendAudioAsVoice: true
+      }
+    );
+
+    await ticket.update({ lastMessage: body || media.filename });
+
+    fs.unlinkSync(media.path);
+
+    return sentMessage;
+  } catch (err) {
+    throw new AppError("ERR_SENDING_WAPP_MSG");
+  }
+};
+
+export default SendWhatsAppMedia;
